04c5353e35219656c6d988cc7151373a5fd96792
Diego <diegocarpbressan@gmail.com>
2019-10-18 09:55:40 -0300

    Se arranca fooooeeeeerteeee

---
+++ TP3 Analisis/Logica/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logica
+{
+    public class Class1
+    {
+        public int Calcular(int[] vecx, int[] vecy, int c)
+        {
+            int acuy = 0;
+            int acux = 0;
+            int acusum = 0;
+            double acuxcuadrado = 0;
+
+            for (int i = 0; i < c; i++)
+            {
+                acux = acux + vecx[i];
+                acuy = acuy + vecy[i];
+
+                acusum = acusum + (vecx[i] * vecy[i]);
+
+                acuxcuadrado = acuxcuadrado + (Math.Pow(vecx[i], 2));
+            }
+        }
+    }
+}
